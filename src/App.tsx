fc1c85233e3370a0e7e4f1401403894865ddd030
Kleber Tiko <klebertiko@gmail.com>
2025-07-08 18:49:36 -0300

    arrumei o projeto, erro de useState gerando um loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [nome, setNome] = useState('')
-  setNome('Air Sense')
+  const [nome, setNome] = useState(''); // Estado inicial vazio ou nulo
+
+  useEffect(() => {
+    // Este código é executado APENAS uma vez após a renderização inicial
+    // por causa do array de dependências vazio `[]`.
+    setNome('Air Sense');
+  }, []); // <--- Array de dependências vazio!
+  
   return (
     <>
       <div>
-        <h1>Olá ${nome}!</h1>
+        <h1>Olá {nome}!</h1>
       </div>
     </>
   )
